b5bb104541fd063c3051b1e9ecb49b8dc1b1faed
Satish Yadav <monstersatish@live.com>
2019-05-19 13:38:47 +0530

    Init

---
+++ CryptoDock/Controllers/HashController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoDock.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoDock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HashController : ControllerBase
+    {
+        // GET: api/Hash/5
+        [HttpGet("{text}", Name = "Get")]
+        public string Get(string text)
+        {
+            return text.ToMd5();
+        }
+
+        // POST: api/Hash
+        [HttpPost]
+        public IDictionary<string, string> Post([FromBody] string[] value)
+        {
+            return value.ToMd5();
+        }
+
+
+    }
+}
---
+++ CryptoDock/Models/Hash.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoDock.Models
+{
+    public static class Hash
+    {
+        public static IDictionary<string, string> ToMd5(this IEnumerable<string> texts)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                var stringAndHash = new ConcurrentDictionary<string, string>();
+                // Convert the input string to a byte array and compute the hash.
+                Parallel.ForEach(texts, (input) =>
+                {
+                    string hash = CreateMd5Hash(input, md5Hash);
+                    stringAndHash.TryAdd(input, hash);
+                });
+                return stringAndHash.ToDictionary(x => x.Key, x => x.Value);
+            }
+        }
+
+        public static string ToMd5(this string input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                return CreateMd5Hash(input, md5Hash);
+            }
+        }
+
+        private static string CreateMd5Hash(string input, MD5 md5Hash)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            var hash = sBuilder.ToString();
+            return hash;
+        }
+    }
+}
